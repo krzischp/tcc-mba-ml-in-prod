d40547476e3c6b9b906fc425227977d5d40f346d
Lucas Lima <64433440+pinholuc@users.noreply.github.com>
2022-10-04 07:58:58 -0300

    Add script that uploads files to gcs

--- gcs/upload_images.py
+++ gcs/upload_images.py
@@ -1,9 +1,29 @@
 """Uploads fashion dataset from Kaggle into Google Cloud Storage"""
 from google.cloud import storage
+from os import listdir
+from os.path import isfile, join
 
 storage_client = storage.Client()
-
 BUCKET_NAME = "tcc-clothes"
 
-for blob in storage_client.list_blobs(BUCKET_NAME, prefix='images/'):
-    print(str(blob))
+bucket = storage_client.get_bucket(BUCKET_NAME)
+only_files = [f for f in listdir("images/images/") if isfile(join("images/images/", f))]
+men_files = [
+    "10268.jpg",
+    "59435.jpg",
+    "22198.jpg",
+    "29570.jpg",
+    "26538.jpg",
+    "49495.jpg",
+    "33822.jpg",
+    "42089.jpg",
+    "49461.jpg",
+    "34835.jpg",
+]
+
+for f in only_files:
+    destination_blob_name = f"images/{f}"
+    source_file_name = f
+    print(f)
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_filename(source_file_name)
