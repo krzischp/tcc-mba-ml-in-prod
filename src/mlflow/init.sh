95738b8ee569ad98a0d0f8db51e3785d8d536b1f
Lucas Lima <64433440+pinholuc@users.noreply.github.com>
2022-10-11 19:01:43 -0300

    Fix Cloud SQL proxy for MLFlow

--- src/mlflow/init.sh
+++ src/mlflow/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./cloud_sql_proxy -instances="${INSTANCE_CONNECTION_NAME}"=tcp0.0.0.0:3306 -credential_file="${GOOGLE_APPLICATION_CREDENTIALS}" &
+./cloud_sql_proxy -instances="${INSTANCE_CONNECTION_NAME}"=tcp:0.0.0.0:3306 -credential_file="${GOOGLE_APPLICATION_CREDENTIALS}" &
 mlflow server -h 0.0.0.0 -p 5000 --default-artifact-root "${MLFLOW_ARTIFACT_URL}" --backend-store-uri "${MLFLOW_DATABASE_URL}"
