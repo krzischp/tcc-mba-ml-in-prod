55f24312e4db6c6c87dc4125409ed3a404dd76bd
Lucas Lima <64433440+pinholuc@users.noreply.github.com>
2022-10-12 18:26:31 -0300

    Add loadbalancers for api and mlflow running in GKE

--- src/mlflow/init.sh
+++ src/mlflow/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export INSTANCE_CONNECTION_NAME=tcc-lucas-pierre:southamerica-east1:mlflow
+export INSTANCE_CONNECTION_NAME="tcc-lucas-pierre:southamerica-east1:mlflow"
 ./cloud_sql_proxy -instances="${INSTANCE_CONNECTION_NAME}"=tcp:0.0.0.0:3306 -credential_file="${GOOGLE_APPLICATION_CREDENTIALS}" &
 mlflow server -h 0.0.0.0 -p 5000 --default-artifact-root "${MLFLOW_ARTIFACT_URL}" --backend-store-uri "${MLFLOW_DATABASE_URL}"
