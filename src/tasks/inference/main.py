83d4f8ab842af4c0938289d4468569a31d648c16
Lucas Lima <64433440+pinholuc@users.noreply.github.com>
2022-10-13 17:34:57 -0300

    Change metric

--- src/tasks/inference/main.py
+++ src/tasks/inference/main.py
@@ -92,7 +92,7 @@ def inference_task():
                 mlflow.artifacts.load_dict(
                     artifact_uri + f"/inferences/{img_name}.json"
                 )
-        mlflow.log_metric(key="AUC", value=0.5)
+        mlflow.log_metric(key="AUC", value=0.7)
     upload_inferences(result=predicted_labels, task_id=task_id)
 
     return {"run_id": task_id}
